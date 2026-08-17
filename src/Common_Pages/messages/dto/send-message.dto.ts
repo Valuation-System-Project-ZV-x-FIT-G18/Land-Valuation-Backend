77d9cb9b6efbc09bca73763dd1782b7e5b58cd85
RadhikaNarampanawa <rasangihmrn@gmail.com>
2026-08-18 00:29:57 +0530

    testfinal

--- src/Common_Pages/messages/dto/send-message.dto.ts
+++ src/Common_Pages/messages/dto/send-message.dto.ts
@@ -3,7 +3,6 @@ import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator'
 
 // Body fields for POST /api/messages (multipart/form-data).
 export class SendMessageDto {
-  @IsString() @MinLength(1) @MaxLength(50) senderId: string
   @IsString() @MinLength(1) @MaxLength(50) recipientId: string
   @IsOptional() @IsString() @MaxLength(10000) body?: string
 }
